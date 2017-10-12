e04dac937b5347475693a7b1d971f6b3662f16bd
Oliver Eriksson <virus.oliver@gmail.com>
2017-10-12 11:17:44 +0200

    Resolved error

--- Garage/Garage/Vehicle.cs
+++ Garage/Garage/Vehicle.cs
@@ -11,5 +11,7 @@ namespace Garage
         string RegNr { get; set; }
         string Model { get; set;}
         DateTime ParkTime { get; set; }
+        string Color { get; set; }
+        string Brand { get; set; }
     }
 }
