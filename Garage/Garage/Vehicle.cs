0c4f0fc3ec38da81b856ce78c833d8da86a65c56
Robert <robertnyquist.rn@gmail.com>
2017-10-12 11:47:27 +0200

    Vehicle enum vtype, tostring

--- Garage/Garage/Vehicle.cs
+++ Garage/Garage/Vehicle.cs
@@ -13,5 +13,25 @@ namespace Garage
         public DateTime ParkTime { get; set; }
         public string Color { get; set; }
         public string Brand { get; set; }
+        public enum Vtype
+        {
+            Bus,
+            Car,
+            Truck,
+            Motorcycle
+        }
+
+        public Vtype v;
+
+        public  string BasicInfo()
+        {
+            return "RegNr: " + RegNr + "\n-Vehicle Type: " + v;
+        }
+        
+        public override string ToString()
+        {
+
+            return "RegNr: " +RegNr +"\n-Brand: " +Brand + "\n--Model: " + Model + "\n---Color: " + Color + "\n----Time Parked: " + ParkTime;
+        }
     }
 }
