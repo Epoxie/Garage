6dfef053e82aa8e4b9b4bf7e2e170b4ac4fcdd13
Oliver Eriksson <virus.oliver@gmail.com>
2017-10-12 10:49:50 +0200

    subclasses added

--- Garage/Garage/Bus.cs
+++ Garage/Garage/Bus.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Garage
 {
-    class Bus
+    class Bus : Vehicle
     {
     }
 }
--- Garage/Garage/Car.cs
+++ Garage/Garage/Car.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Garage
 {
-    class Car
+    class Car : Vehicle
     {
     }
 }
